a65ddd1038ff2e7313bcd76fc0e1efe713f0898d
idossha <idochaber@gmail.com>
2026-01-25 17:22:41 -0600

    combined dicon2nifti and fix t2 scripts

--- tit/pre/__init__.py
+++ tit/pre/__init__.py
@@ -11,7 +11,6 @@ from .dicom2nifti import run_dicom_to_nifti
 from .recon_all import run_recon_all
 from .charm import run_charm
 from .tissue_analyzer import run_tissue_analysis
-from .fix_t2_filenames import run_fix_t2_filenames
 from .qsi import run_qsiprep, run_qsirecon, extract_dti_tensor
 
 __all__ = [
@@ -20,7 +19,6 @@ __all__ = [
     "run_recon_all",
     "run_charm",
     "run_tissue_analysis",
-    "run_fix_t2_filenames",
     "run_qsiprep",
     "run_qsirecon",
     "extract_dti_tensor",
