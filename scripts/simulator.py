d34ae999b8abb99ba9cb38c6eec47024a1eff362
idossha <idochaber@gmail.com>
2026-03-03 18:50:45 -0600

    new scripting approach instead of cli

---
+++ scripts/simulator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env simnibs_python
+
+from tit.sim import *
+
+PROJECT_DIR = "/mnt/000/"      
+SUBJECT_ID  = "101"    
+
+config = SimulationConfig(
+    subject_id=SUBJECT_ID,
+    project_dir=PROJECT_DIR,
+
+    conductivity_type=ConductivityType.SCALAR,
+
+    intensities=IntensityConfig(pair1=1.0, pair2=1.0),
+
+    electrode=ElectrodeConfig(
+        shape="ellipse",
+        dimensions=[8.0, 8.0],   
+        thickness=4.0,            
+        sponge_thickness=2.0)
+
+montages = load_montages(
+    montage_names=["L_Insula"],
+    project_dir=PROJECT_DIR,
+    eeg_net="GSN-HydroCel-185.csv")
+
+results = run_simulation(config, montages)
