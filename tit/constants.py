487a44a2653422298bfb14129ed66a1838d9947e
idossha <idochaber@gmail.com>
2026-03-24 19:05:20 -0500

    upgraded qsi to version 1.2.0

--- tit/constants.py
+++ tit/constants.py
@@ -322,7 +322,7 @@ PATTERN_COORDINATE = r"^-?\d+\.?\d*$"
 # QSI Docker images
 QSI_QSIPREP_IMAGE = "pennlinc/qsiprep"
 QSI_QSIRECON_IMAGE = "pennlinc/qsirecon"
-QSI_DEFAULT_IMAGE_TAG = "1.1.1"
+QSI_DEFAULT_IMAGE_TAG = "1.2.0"
 
 # QSI directories
 DIR_QSIPREP = "qsiprep"
